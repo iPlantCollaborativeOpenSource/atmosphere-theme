def8e0c6ea66637531703c8ff68d68d76e118370
Tharon Calrson <tharonica@gmail.com>
2015-08-12 09:27:38 -0700

    adding browsersync and autoprefixer to gulp taskes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
-    prefix = require('gulp-autoprefixer'),
-    sass = require('gulp-sass');
+  prefix = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync');
 
 gulp.task('style', function() {
   return gulp.src('./sass/*.scss')
@@ -9,8 +10,15 @@ gulp.task('style', function() {
     .pipe(gulp.dest('./css/'));
 });
 
+gulp.task('browser-sync', function() {
+  browserSync.init({
+    proxy: "yourlocal.dev"
+  });
+});
+
 // default gulp watch task
 gulp.task('default', function() {
   var client = ['style'];
   gulp.watch ('./sass/*.scss' , client);
+  gulp.watch ('./*.css');
 });
